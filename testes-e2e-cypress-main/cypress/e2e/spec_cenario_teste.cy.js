621fabe5b5a6d1c7e7e532eca833d94c31d8b185
fariasleticia <leticiafcassimiro@gmail.com>
2024-08-15 15:24:05 -0300

    Realização de testes na página inicial

--- testes-e2e-cypress-main/cypress/e2e/spec_cenario_teste.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/spec_cenario_teste.cy.js
@@ -3,3 +3,10 @@ describe('template spec', () => {
     cy.visit('localhost:3000')
   })
 })
+
+describe('Página Principal', () => {
+  it('Deve renderizar h1 com o texto correto!', () => {
+    cy.visit('http://localhost:3000')
+    cy.get('h1').contains('Experimente mais liberdade no controle da sua vida financeira. Crie sua conta com a gente!')
+  })
+})
