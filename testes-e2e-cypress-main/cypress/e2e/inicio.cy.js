258a7237f8f70124e773d5ed56ae4b397a4622ad
fariasleticia <leticiafcassimiro@gmail.com>
2024-08-16 11:41:18 -0300

    Teste de elementos de título, texto e imagem

--- testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/inicio.cy.js
@@ -8,7 +8,36 @@ describe('Página Principal', () => {
   it('Deve renderizar h2 com o texto correto!', () => {
     cy.getByData('titulo-vantagens').contains('Vantagens do nosso banco:')
   })
+
+  it('Deve renderizar h3 - vantagem 1 - com o subtítulo correto!', () => {
+    cy.getByData('subtitulo1').contains('Conta e cartão gratuitos')
+  })
+  it('Deve renderizar parágrafo - vantagem 1 - com o texto correto!', () => {
+    cy.getByData('vantagem1').contains('Isso mesmo, nossa conta é digital, sem custo fixo e mais que isso: sem tarifa de manutenção.')
+  })
+
+  it('Deve renderizar h3 - vantagem 2 - com o subtítulo correto!', () => {
+    cy.getByData('subtitulo2').contains('Saques sem custo')
+  })
+  it('Deve renderizar parágrafo - vantagem 2 - com o texto correto!', () => {
+    cy.getByData('vantagem2').contains('Você pode sacar gratuitamente 4x por mês de qualquer Banco 24h.')
+  })
+
+  it('Deve renderizar h3 - vantagem 3 - com o subtítulo correto!', () => {
+    cy.getByData('subtitulo3').contains('Programa de pontos')
+  })
   it('Deve renderizar o parágrafo com o texto correto!', () => {
     cy.getByData('vantagem3').contains('Você pode acumular pontos com suas compras no crédito sem pagar mensalidade!')
+  })
+
+  it('Deve renderizar h3 - vantagem 4 - com o subtítulo correto!', () => {
+    cy.getByData('subtitulo4').contains('Seguro Dispositivos')
+  })
+  it('Deve renderizar o parágrafo com o texto correto!', () => {
+    cy.getByData('vantagem4').contains('Seus dispositivos móveis (computador e laptop) protegidos por uma mensalidade simbólica.')
+  })
+  
+  it('Deve renderizar todas as imagens', () => {
+    cy.getByData('imagem').should("be.visible")
   })
 })
--- testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
+++ testes-e2e-cypress-main/src/paginas/Inicio/index.jsx
@@ -17,6 +17,7 @@ export default function Inicio() {
               Crie sua conta com a gente!
             </h1>
             <img
+              data-test="imagem"
               src={ilustracaoBanner}
               alt="Gráficos e uma pessoa com dinheiro na mão"
             />
@@ -25,24 +26,24 @@ export default function Inicio() {
             <h2 data-test="titulo-vantagens">Vantagens do nosso banco:</h2>
             <div className={estilos.vantagens__itens}>
               <div className={estilos.vantagens__item}>
-                <img src={presente} alt="ícone de um presente" />
-                <h3>Conta e cartão gratuitos</h3>
-                <p>
+                <img src={presente} data-test="imagem" alt="ícone de um presente" />
+                <h3 data-test="subtitulo1">Conta e cartão gratuitos</h3>
+                <p data-test="vantagem1">
                   Isso mesmo, nossa conta é digital, sem custo fixo e mais que
                   isso: sem tarifa de manutenção.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img src={saque} alt="ícone de uma mão segurando um dinheiro" />
-                <h3>Saques sem custo</h3>
-                <p>
+                <img src={saque} data-test="imagem" alt="ícone de uma mão segurando um dinheiro" />
+                <h3 data-test="subtitulo2">Saques sem custo</h3>
+                <p data-test="vantagem2">
                   Você pode sacar gratuitamente 4x por mês de qualquer Banco
                   24h.
                 </p>
               </div>
               <div className={estilos.vantagens__item}>
-                <img src={estrela} alt="ícone de uma estrela" />
-                <h3>Programa de pontos</h3>
+                <img src={estrela} data-test="imagem" alt="ícone de uma estrela" />
+                <h3 data-test="subtitulo3">Programa de pontos</h3>
                 <p data-test="vantagem3">
                   Você pode acumular pontos com suas compras no crédito sem
                   pagar mensalidade!
@@ -50,11 +51,12 @@ export default function Inicio() {
               </div>
               <div className={estilos.vantagens__item}>
                 <img
+                  data-test="imagem"
                   src={dispositivo}
                   alt="ícone de um dispositivo como notebook"
                 />
-                <h3>Seguro Dispositivos</h3>
-                <p>
+                <h3 data-test="subtitulo4">Seguro Dispositivos</h3>
+                <p data-test="vantagem4">
                   Seus dispositivos móveis (computador e laptop) protegidos por
                   uma mensalidade simbólica.
                 </p>
