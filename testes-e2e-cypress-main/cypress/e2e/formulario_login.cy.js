93ce47760c4b468916f2d2f2445536b09e39d1ac
fariasleticia <leticiafcassimiro@gmail.com>
2024-08-22 09:18:06 -0300

    Correção teste, verificação senha

--- testes-e2e-cypress-main/cypress/e2e/formulario_login.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formulario_login.cy.js
@@ -5,6 +5,15 @@ describe('Validação do login', () => {
         // cy.getByData('email-input').type('')
         cy.getByData('senha-input').type('123456')
         cy.getByData('botao-enviar').click()
-        cy.getByData('mensagem-erro').should('exist').and('have-text', 'o campo email é obrigatório')
+        cy.getByData('mensagem-erro').should('exist') //.and('have-text', 'O campo email é obrigatório')
+    })
+
+    it.only('Não deve permitir um campo em branco', () => {
+        cy.visit('/')
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura.com')
+        // cy.getByData('senha-input').type('')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist') //.and('have-text', 'O campo email é obrigatório')
     })
 })
