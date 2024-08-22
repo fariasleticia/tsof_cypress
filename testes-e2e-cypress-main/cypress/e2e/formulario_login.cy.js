ff14a2535c8d3c7b0a90afe61ec1c32e7803d337
fariasleticia <leticiafcassimiro@gmail.com>
2024-08-22 10:30:30 -0300

    Correção e teste

--- testes-e2e-cypress-main/cypress/e2e/formulario_login.cy.js
+++ testes-e2e-cypress-main/cypress/e2e/formulario_login.cy.js
@@ -1,19 +1,29 @@
-describe('Validação do login', () => {
-    it.only('Não deve permitir um campo em branco', () => {
-        cy.visit('/')
+describe('Formulário login', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+    })
+
+    it('Não deve permitir um e-mail inválido!', () => {
+        cy.getByData('botao-login').click()
+        cy.getByData('email-input').type('neilton@alura')
+        cy.getByData('senha-input').type('123456')
+        cy.getByData('botao-enviar').click()
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O email digitado é inválido')
+    })
+
+    it('Não deve permitir o campo email em branco', () => {
         cy.getByData('botao-login').click()
         // cy.getByData('email-input').type('')
         cy.getByData('senha-input').type('123456')
         cy.getByData('botao-enviar').click()
-        cy.getByData('mensagem-erro').should('exist') //.and('have-text', 'O campo email é obrigatório')
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo email é obrigatório')
     })
 
-    it.only('Não deve permitir um campo em branco', () => {
-        cy.visit('/')
+    it('Não deve permitir o campo senha em branco', () => {
         cy.getByData('botao-login').click()
         cy.getByData('email-input').type('neilton@alura.com')
         // cy.getByData('senha-input').type('')
         cy.getByData('botao-enviar').click()
-        cy.getByData('mensagem-erro').should('exist') //.and('have-text', 'O campo email é obrigatório')
+        cy.getByData('mensagem-erro').should('exist').and('have.text', 'O campo de senha é obrigatório')
     })
 })
